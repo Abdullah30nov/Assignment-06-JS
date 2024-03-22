b9f948ba6fad0aaa4a36e0e66cd8e23b11364097
Abdullah30nov <abdullahbinaslam64@gmail.comgit config --global user.email abdullahbinaslam64@gmail.com>
2024-03-21 23:26:18 -0700

    Udload VIA code

---
+++ app.js
@@ -0,0 +1,21 @@
+                            //  QUESTION-NO-01
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var fullName = firstName + " " + lastName;
+// document.write("Your fullName is: " + fullName);
+
+
+                            //   QUESTION-NO-02
+// var favphone = prompt("Enter your favorite mobile & mobile model: ");
+// document.write("Your favorite phone is: "+ favphone +"<br>"+"Length of string: "+favphone.length);
+
+
+                                //   QUESTION-NO-03
+// var input = prompt("Enter any thing I found 'N'")
+//       var text = input.toLowerCase();
+// for(var i = 0; i<text.length;i++)
+// {
+//     if(text.slice(i,i+2) === "n"){
+//         alert("Found 'N'")
+//     }
+// }
